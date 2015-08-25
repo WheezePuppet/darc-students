c7328dd7ab4957e123ed2b69220c1f9fbd1e149a
Stephen Davies <stephen@umw.edu>
2015-08-25 02:26:23 -0400

    Add episode VI scenes.

--- expand.sh
+++ expand.sh
@@ -9,6 +9,7 @@ else
     else 
         sed -i 's/\<obi\>/Obi-wan/g' $1
         sed -i 's/\<dv\>/Vader/g' $1
+        sed -i 's/\<DV\>/Vader/g' $1
         sed -i 's/\<tar\>/Tarkin/g' $1
         sed -i 's/\<luke\>/Luke/g' $1
         sed -i 's/\<leia\>/Leia/g' $1
@@ -24,6 +25,9 @@ else
         sed -i 's/\<wedge\>/Wedge/g' $1
         sed -i 's/\<yoda\>/Yoda/g' $1
         sed -i 's/\<jab\>/Jabba/g' $1
+        sed -i 's/\<bib\>/Bib Fortuna/g' $1
+        sed -i 's/\<oola\>/Oola/g' $1
+        sed -i 's/\<droidBoss\>/Droid Boss/g' $1
         sed -i 's/\<emp\>/Emperor/g' $1
         sed -i 's/\<lobot\>/Lobot/g' $1
         sed -i 's/\<snag\>/Snaggletooth/g' $1
@@ -37,9 +41,18 @@ else
         sed -i 's/\<hoth\>/Hoth/g' $1
         sed -i 's/\<mf\>/Falcon/g' $1
         sed -i 's/\<ds\>/Death Star/g' $1
+        sed -i 's/\<DS\>/Death Star/g' $1
         sed -i 's/\<tat\>/Tatooine/g' $1
         sed -i 's/\<mos\>/Mos Eisley/g' $1
         sed -i 's/\<lando\>/Lando/g' $1
+        sed -i 's/\<abity\>/Nien Nunb/g' $1
         sed -i 's/\<boba\>/Boba Fett/g' $1
+        sed -i 's/\<palace\>/Jabbas Palace/g' $1
+        sed -i 's/\<mothma\>/Mon Mothma/g' $1
+        sed -i 's/\<madine\>/Madine/g' $1
+        sed -i 's/\<ackbar\>/Ackbar/g' $1
+        sed -i 's/\<ack\>/Ackbar/g' $1
+        sed -i 's/\<wicket\>/Wicket/g' $1
+        sed -i 's/\<endor\>/Endor/g' $1
     fi
 fi
