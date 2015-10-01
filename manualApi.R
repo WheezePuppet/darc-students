1437b1765d9bda2eb79b02e596c078e35520e8b6
Stephen <stephen@umw.edu>
2015-10-01 00:05:29 -0400

    Add boilerplate code for manual Twitter API.

---
+++ manualApi.R
@@ -0,0 +1,29 @@
+
+library(httr)
+library(jsonlite)
+
+source("twitterLogonInfo.R")
+
+
+auth <- paste("Basic",RCurl::base64(paste(key,secret,sep=":")))
+auth.req <- POST("https://api.twitter.com/oauth2/token",
+    add_headers(
+        "Authorization"=auth,
+        "Content-Type"="application/x-www-form-urlencoded;charset=UTF-8"),
+    body="grant_type=client_credentials")
+
+access.token <- paste("Bearer",content(auth.req)$access_token)
+
+
+make.manual.twitter.api.call <- function(the.api.request) {
+    return(
+        fromJSON(
+            content(
+                GET(the.api.request,add_headers(Authorization=access.token)),
+                as="text")
+        )
+    )
+}
+
+
+hannahs.info <- make.manual.twitter.api.call("https://api.twitter.com/1.1/users/lookup.json?screen_name=hzontine")
---
+++ twitterLogonInfo.R
@@ -0,0 +1,26 @@
+
+# Reminder: you have to create an "app" so that you can connect to the API.
+# (You're not really creating an app, it's more of just a connection
+# point.) If you still have your info from before, that's fine, you don't need
+# to create another app.
+#
+# To create an app and get the info:
+#
+# Go to http://dev.twitter.com, and make an account if you don't have one.
+#
+#   Click on "Manage your Apps" at the very bottom.
+#
+#   Create a New App. Important: the "Callback URL" must be this
+#   (copy/paste it): http://127.0.0.1:1410
+#
+#   When the app is created, you should be able to go to a "Keys and
+#   Access Tokens" tab. The two pieces of info you need are:
+#
+#   1. the "key" (also called "consumer key")
+#   2. the "secret" (also called "consumer secret")
+#
+# Paste those below.
+
+key <- "your key here"
+secret <- "your secret here"
+
