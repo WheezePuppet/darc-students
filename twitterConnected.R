969e47c221b7b3b6e3e54a96ee7dcd3c2e66b734
hzontine <hzontine@gmail.com>
2015-10-18 14:49:55 -0400

    Added one way twitter connection

--- twitterConnected.R
+++ twitterConnected.R
@@ -9,6 +9,17 @@ are.twitter.connected <- function(user.id.1, user.id.2){
   }
 }
 
+are.one.way.twitter.connected <- function(user.id.1, user.id.2){
+  call <- paste("https://api.twitter.com/1.1/friendships/show.json?source_id=",user.id.1,"&target_id=",user.id.2, sep="")
+  answer <- make.manual.twitter.api.call(call)
+  if (answer$relationship$target$following == TRUE){
+    return (TRUE)
+  }
+  else{
+    return (FALSE)
+  }
+}
+
 are.twitter.double.connected <- function(user.id.1, user.id.2){
   call <- paste("https://api.twitter.com/1.1/friendships/show.json?source_id=",user.id.1,"&target_id=",user.id.2, sep="")
   answer <- make.manual.twitter.api.call(call)
