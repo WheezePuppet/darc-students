f24c18be1c1e4a712dcf8bca3a5b36751d821ee7
Hannah Zontine <hzontine@Hannahs-MBP.home>
2015-11-15 22:35:33 -0500

    Fixed inducedsubgraphs.R

--- inducedsubgraphs.R
+++ inducedsubgraphs.R
@@ -5,7 +5,7 @@ induced.subgraph.at.time.t <- function(g, N, t){
   attribute <- N$attr$timestamps
   raw.graph <- induced_subgraph(g, which(V(g)$name %in% tweeters), impl= c("auto"))
   raw.graph$attr <- attribute
-  V$raw.graph$name <- tweeters[]
+  V(raw.graph)$name <- tweeters
   return (raw.graph)
 }
 
@@ -14,11 +14,11 @@ induced.subgraph.up.to.time.t <- function(g, N, t){
   attribute <- N$attr$timestamps
   cooked.graph <- induced_subgraph(g, which(V(g)$name %in% tweeters), impl = c("auto"))
   cooked.graph$attr <- attribute
-  V$cooked.graph$name <- tweeters[]
+  V(cooked.graph)$name <- tweeters[]
   return (cooked.graph)
 }
 
 
-#N <- list(c("Leia", "Han", "Wedge"), c("Leia", "Emperor", "Luke"), c("Lando", "Han", "Luke"), c("Jerjerrod", "Greedo", "Lando", "Han", "Yoda"), c("Yoda", "Needa", "Luke", "Vader", "Biggs", "Owen"))
-#plot(induced.subgraph.up.to.time.t(mentions, N, 2))
+N <- list(c("Leia", "Han", "Wedge"), c("Leia", "Emperor", "Luke"), c("Lando", "Han", "Luke"), c("Jerjerrod", "Greedo", "Lando", "Han", "Yoda"), c("Yoda", "Needa", "Luke", "Vader", "Biggs", "Owen"))
+plot(induced.subgraph.up.to.time.t(mentions, N, 2))
 #plot(induced.subgraph.at.time.t(mentions, N, 5))
