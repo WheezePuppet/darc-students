25927644d12511084798c52c5b05c40a1274744b
Hannah Zontine <hzontine@Hannahs-MacBook-Pro.local>
2015-11-17 20:13:30 -0500

    Finished inducedsubgraphs

--- inducedsubgraphs.R
+++ inducedsubgraphs.R
@@ -5,46 +5,36 @@ induced.subgraph.at.time.t <- function(g, N, t){
   tweeter <- N$graph[[t]]$tweeter
   followers <- N$graph[[t]]$followers
   timestamp <- N$attribute[[t]]$tweet.date
-  #raw.graph <- induced_subgraph(g, which(V(g)$name %in% tweeters), impl= c("auto"))
   vec<- c(pwners$graph[[t]]$tweeter, pwners$graph[[t]]$followers)
   raw.graph <- induced_subgraph(g, vec, impl=c("auto"))
  
-  #Bad way
-  #raw.graph$attr <- timestamp
-  #Good way
-  #If we want timestamp of the tweeter's first tweet
+  #Timestamp of the t'th tweeter's tweet
   raw.graph <- set_graph_attr(raw.graph, "Time", timestamp)
 
-  #Bad way
-  #V(raw.graph)$name <- tweeter
-  #Good way
+  #Twitter ID of the t'th tweeter
   raw.graph <- set_graph_attr(raw.graph, "Tweeter", tweeter)
 
   return (raw.graph)
 }
+
+
 #Creates a subgraph of graph g using the vertices from N from tweeter 1 through t
 induced.subgraph.up.to.time.t <- function(g, N, t){
   tweeter<- c()
   followers<- vector("list")
   timestamp<- c()
-  x <- 1:t
-  for (i in x){
+  num <- t
+  for (i in 1:num){
 	tweeter[i] <- N$graph[[i]]$tweeter
-#	followers[i] <- N$graph[[i]]$followers
+	followers <- c(followers, N$graph[[i]]$followers)
  	timestamp[i] <- N$attribute[[i]]$tweet.date
   }
+  followers <- unlist(followers)
   vec <- c(tweeter, followers)
-  cooked.graph <- induced_subgraph(g, vec, impl=c("auto"))
+  cooked.graph <- induced_subgraph(g, vec, impl=c("copy_and_delete"))
 
-  #Do we want time and tweeter attributes here?
+  #Do we actually want tweeter and tweet date for this function?
   
-
-
-  #tweeters<- unique(unlist(N$g[1:t]))
-  #timestamps <- N$attribute[[t]]$tweet.date
-  #cooked.graph <- induced_subgraph(g, which(V(g)$name %in% tweeters), impl = c("auto"))
-  #cooked.graph$attr <- timestamps
-  #V(cooked.graph)$name <- tweeters[]
   return (cooked.graph)
 }
 
