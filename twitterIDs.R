eced2fb8ddfe272c0f91e84ac36fad7fbc8eb33e
Livie <oking@mail.umw.edu>
2015-11-22 16:56:44 -0500

    nope

--- twitterIDs.R
+++ twitterIDs.R
@@ -1,1 +1,3 @@
-
+# nope, can't figure it out
+# but, just for the sake of sharing knowledge, I'll record my current completed lists:
+#  #AisMona, #AlexFromTarget, #AskObama, #BeABetterMan, #BlackLivesMatter, #DeadRaccoontTO, #McDStories, #re2pect, #WhyImNotVotingForHillary, yolo
