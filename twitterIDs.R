012080742223a9059eec9fa0b666f38062b7ed66
Livie <oking@mail.umw.edu>
2015-12-02 17:32:43 -0500

    Delete twitterIDs.R

--- twitterIDs.R
+++
@@ -1,3 +0,0 @@
-# nope, can't figure it out
-# but, just for the sake of sharing knowledge, I'll record my current completed lists:
-#  #AisMona, #AlexFromTarget, #AskObama, #BeABetterMan, #BlackLivesMatter, #DeadRaccoontTO, #McDStories, #re2pect, #WhyImNotVotingForHillary, yolo
