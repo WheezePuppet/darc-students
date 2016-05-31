720ed6be838ef882f6a3d8295158b619dd60e75b
Stephen <stephen@umw.edu>
2016-05-31 13:27:46 -0400

    Add Katz centrality.

--- sw/centrality.R
+++ sw/centrality.R
@@ -10,6 +10,9 @@ plot.sw.graph(scenes,"Scenes")
 # Get the adjacency matrix. (as.matrix() to make non-sparse.)
 A <- as.matrix(get.adjacency(scenes))
 
+
+# I. Perron-Frobenius centrality.
+
 # The Perron-Frobenius eigenvector is the eigenvector that goes with the
 # highest eigenvalue of the adjacency matrix. (Take the abs() because this
 # vector may be all-negative instead of all-positive.)
@@ -23,3 +26,34 @@ print(most.central.chars)
 
 cat("\nevcent says:\n")
 print(sort(evcent(scenes)$vector,decreasing=TRUE),3)
+
+
+cat("\n\n")
+
+
+# II. Katz centrality.
+
+# Compute the maximum "legal" value of alpha.
+max.alpha <- 1/eigen(A)$values[1]
+
+# For no particularly good reason, choose half that value as our alpha.
+alpha <- max.alpha / 2
+
+# Compute the "centrality matrix" Q, for this alpha.
+Q <- solve(diag(vcount(scenes)) - alpha * A)
+
+# Compute the "broadcast centality vector" b, which is Q's row sums.
+b <- Q %*% rep(1,vcount(scenes))
+most.central.broadcasters <- round(sort(abs(b),decreasing=TRUE),3)
+names(most.central.broadcasters) <- V(scenes)[order(b,decreasing=TRUE)]$name
+
+# Compute the "receive centality vector" r, which is Q's col sums.
+r <- t(Q) %*% rep(1,vcount(scenes))
+most.central.receivers <- round(sort(abs(r),decreasing=TRUE),3)
+names(most.central.receivers) <- V(scenes)[order(r,decreasing=TRUE)]$name
+
+cat("\nKatz centrality, alpha = ", round(alpha,4), ":\n", sep="")
+cat("Broadcasters:\n")
+print(sort(most.central.broadcasters,decreasing=TRUE))
+cat("Receivers:\n")
+print(sort(most.central.receivers,decreasing=TRUE))
